201f757a6fed60019ade234fbc86192e1d04b30c
zhengguizhou <zhengguizhou@gridsum.com>
2021-12-22 09:50:19 +0800

    完成基础配置

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 import './main.css';
+import './sass.scss';
+import logo from '../public/1.png'
+
+
+// require('./main.css');
+// require('./sass.scss');
+// const logo = require('../public/1.png');
+
+const dom = document.createElement('span');
+document.body.appendChild(dom);
+
+const img = new Image();
+img.src = logo
+
+console.log(document.getElementById('imgBox'));
+console.log(img);
+document.getElementById('imgBox').appendChild(img)
+
+class Author {
+  name = 'ITEM'
+  age = 18
+  email = 'lxp_work@163.com'
+
+  info =  () => {
+    return {
+      name: this.name,
+      age: this.age,
+      email: this.email
+    }
+  }
+}
+
+
+// export default Author;
 
 const a = 'Hello webpack';
 console.log(a);
-module.exports = a;
+export default Author;
+
+// module.exports = a;
--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,67 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 console.log('process.env.NODE_ENV', process.env.NODE_ENV); // 打印环境变量
 
 const config = {
-  mode:'development', // 开发模式
+  mode:'none', // 开发模式
   // entry: path.resolve(__dirname,'./src/index.js'),    // 入口文件
-  entry: path.resolve(__dirname,'./src/main.css'),    // 入口文件
+  // entry: path.resolve(__dirname,'./src/main.css'),    // 入口文件
+  // output: {
+  //   filename: 'bundle.css',      // 打包后的文件名称
+  //   path: path.resolve(__dirname,'./dist')  // 打包后的目录
+  // },
+  entry: path.resolve(__dirname,'./src/index.js'),    // 入口文件
+  // entry: path.resolve(__dirname,'./src/main.css'),    // 入口文件
   output: {
-    filename: 'bundle.css',      // 打包后的文件名称
+    filename: 'bundle.js',      // 打包后的文件名称
     path: path.resolve(__dirname,'./dist')  // 打包后的目录
   },
   module: {
     rules: [  // 转换规则
       {
-        test: /\.css$/, // 匹配所有css文件
-        use: ['style-loader','css-loader'] // use: 对应的loader名称
+        test: /\.(s[ac]|c)ss$/, // 匹配所有css文件
+        use: [
+          // 'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ] // use: 对应的loader名称
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        use: [
+          'file-loader',
+        ]
+      },
+      // {
+      //   test: /\.(jpe?g|png|gif)$/i,
+      //   type: 'asset',
+      //   generator: {
+      //     filename: '[name][hash:8][ext]'
+      //   },
+      //   parser: {
+      //     dataUrlCondition: {
+      //       maxSize: 10 * 1024 // 超过100kb不转 base64
+      //     }
+      //   }
+      // }
+      {
+        test: /\.js$/i,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env'
+              ]
+            }
+          }
+        ]
       }
     ]
   },
@@ -26,11 +71,15 @@ const config = {
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash:8].css'
+    })
+
   ],
   devServer: {
     // contentBase: path.resolve(__dirname, 'public'), // 静态文件目录，本地开发时不需要copy
-    compress: true, // 是否启动压缩gzip
+    // compress: true, // 是否启动压缩gzip
     port: 8082, // 端口号
   }
 }
